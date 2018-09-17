750b2cef5402cf78bf4f4d8e4b28fa4d947e785d
Paulo Leitao <paulojbleitao@gmail.com>
2018-09-17 15:17:54 -0300

    exercicio de lambda calculus finalizado

--- 04-lambda.hs
+++ 04-lambda.hs
@@ -6,32 +6,52 @@ square = \x -> x*x
 fix :: (a -> a) -> a
 fix f = f (fix f)
 
-pow = \x y -> if (y == 0) then 1 else x * pow x (y - 1)
+pow = fix (\f x y -> if (y == 0) then 1 else x * f x (y - 1))
 fatorial = \x -> product [1..x] 
 isPrime = \x -> (length $ filter (\y -> x `mod` y == 0) [1..x]) == 2
-fib = \x -> if (x == 1 || x == 2) then 1 else fib (x - 1) + fib (x - 2)
-mdc = \x y -> if (x == 0) then y else if (y == 0) then x else mdc y (x `mod` y)
+fib = fix (\f x -> if (x == 1 || x == 2) then 1 else f (x - 1) + f (x - 2))
+mdc = fix (\f x y -> if (x == 0) then y else if (y == 0) then x else f y (x `mod` y))
 mmc x y = \x y -> x * y `div` mdc x y
 coprimo = \x y -> mdc x y == 1
 goldbach x = undefined
 
 --Implemente as funções sobre listas escritas previsamente usando expressões lambda
 --consulte suas implementacoes anteriores para a documentacao dessas funcoes
-meuLast = \xs -> if (length xs == 0) then error "Lista vazia!" else if (length xs == 1) then head xs else meuLast $ tail xs
-penultimo = \xs -> if (length xs == 0 || length xs == 1) then error "Lista sem penultimo" else if (length xs == 2) then head xs else penultimo $ tail xs
-elementAt i xs = undefined
-meuLength xs = undefined
-meuReverso xs = undefined
-isPalindrome xs = undefined
-compress xs = undefined
-compact xs = undefined
-encode xs = undefined
-split xs i = undefined
-slice xs imin imax = undefined
-insertAt el pos xs = undefined
+meuLast = fix (\f xs -> if (length xs == 0) then error "Lista vazia!" else if (length xs == 1) then head xs else f $ tail xs)
+penultimo = fix (\f xs -> if (length xs == 0 || length xs == 1) then error "Lista sem penultimo" else if (length xs == 2) then head xs else f $ tail xs)
+elementAt = fix(\f i xs -> if (i == 1) then head xs else f (i - 1) (tail xs))
+meuLength = \xs -> sum [1 | x <- xs]
+meuReverso = \xs -> if (length xs == 0) then [] else meuReverso (tail xs) ++ [head xs]
+
+isPalindrome :: [Int] -> Bool
+isPalindrome = \xs -> xs == (meuReverso xs)
+
+compress :: [Int] -> [Int]
+compress = fix (\f xs -> if (length xs == 0) then [] else if (head xs `elem` tail xs) then f (tail xs) else [head xs] ++ f (tail xs))
+
+compact :: [Int] -> [Int]
+compact = fix (\f xs -> if (length xs == 0) then [] else filter (== head xs) xs ++ f (filter (/= head xs) (tail xs)))
+
+encode :: [Int] -> [(Int, Int)]
+encode = fix (\f xs -> if (length xs == 0) then [] else [(head xs, length $ filter (== head xs) xs)] ++ f (filter (/= head xs) (tail xs)))
+
+split = \xs i-> [take i xs, drop i xs]
+slice = \xs imin imax -> [xs !! i | i <- [(imin - 1)..(imax - 1)]]
+insertAt = \el pos xs -> (split xs (pos - 1)) !! 0 ++ [el] ++ (split xs (pos - 1)) !! 1
+
 sort xs = undefined
-mySum xs = undefined
-maxList xs = undefined
-buildPalindrome xs = undefined
-mean xs = undefined
-myAppend xs ys = undefined
+
+mySum :: [Int] -> Int
+mySum = \xs -> foldr (+) 0 xs
+
+maxList :: [Int] -> Int
+maxList = \(x:xs) -> foldr max x (x:xs)
+
+buildPalindrome :: [a] -> [a]
+buildPalindrome = fix (\f xs -> if (length xs == 0) then [] else [head xs] ++ f (tail xs) ++ [head xs])
+
+mean :: [Int] -> Int
+mean = \xs -> sum xs `div` length xs
+
+myAppend :: [a] -> [a] -> [a]
+myAppend = \xs ys -> foldr (:) ys xs
