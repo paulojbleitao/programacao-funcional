2652862ba92f7127e0e951d8dbfe89d35d778bf2
paulojbleitao <paulojbleitao@gmail.com>
2018-11-12 21:27:29 -0300

    finalizada implementaçao de multiset com map

--- 07-multiset-map.hs
+++ 07-multiset-map.hs
@@ -36,20 +36,36 @@ remove elem bag
 {-
  - Busca um elemento na estrutura retornando sua quantidade. Caso o elemento nao exista, retorna 0 como a quantidade.
 -}
-search elem bag = undefined
+search :: Int -> Map Int Int -> Int
+search elem bag
+    | quantity /= Nothing = fromJust quantity
+    | otherwise           = 0
+    where quantity = Map.lookup elem bag
 
 {-
  - Faz a uniao deste Bag com otherBag. A uniao consiste em ter os elementos dos dois Bags com suas maiores quantidades.
  - Por exemplo, A = {(a,1),(c,3)}, B = {(b,2),(c,1)}. A.union(B) deixa A = {(a,1),(c,3),(b,2)}
 -}
-union bag1 bag2 = undefined
+union :: Map Int Int -> Map Int Int -> Map Int Int
+union bag1 bag2 = unionWith union' bag1 bag2
+
+union' :: Int -> Int -> Int
+union' e1 e2
+    | e1 >= e2  = e1
+    | otherwise = e2
 
 {-
  - Faz a intersecao deste Bag com otherBag. A intersecao consiste em ter os elementos que estao em ambos os bags com suas 
  - menores quantidades. Por exemplo, Seja A = {(a,3),(b,1)} e B = {(a,1)}. Assim, A.intersection(B) deixa A = {(a,1)}
  - Caso senhum elemento de A esteja contido em B ent�o a intersecao deixa A vazio.
 -}
-intersection bag1 bag2 = undefined
+intersection :: Map Int Int -> Map Int Int -> Map Int Int
+intersection bag1 bag2 = intersectionWith intersection' bag1 bag2
+
+intersection' :: Int -> Int -> Int
+intersection' e1 e2
+    | e1 >= e2  = e2
+    | otherwise = e1
 
 {-
  - Faz a diferenca deste Bag com otherBag. A diferenca A \ B entre bags eh definida como segue:
@@ -58,20 +74,35 @@ intersection bag1 bag2 = undefined
      Caso essa quantidade seja negativa o elemento deve serremovido do Bag. 
      Por exemplo, seja A = {(a,3),(b,1)} e B = {(b,2),(a,1)}. Assim, A.minus(B) deixa A = {(a,2)}.
 -}
-minus bag1 bag2 = undefined
+minus :: Map Int Int -> Map Int Int -> Map Int Int
+minus bag1 bag2 = differenceWith minus' bag1 bag2
+
+minus' :: Int -> Int -> Maybe Int
+minus' e1 e2
+    | difference > 0 = Just difference
+    | otherwise      = Nothing
+    where difference = e1 - e2
 
 {-
  - Testa se este Bag esta incluso em otherBag. Para todo elemento deste bag, sua quantidade
  - deve ser menor or igual a sua quantidade em otherBag.
 -}
-inclusion bag1 bag2 = undefined
+inclusion :: Map Int Int -> Map Int Int -> Bool
+inclusion bag1 bag2 = isSubmapOfBy inclusion' bag1 bag2
+
+inclusion' :: Int -> Int -> Bool
+inclusion' e1 e2
+    | e1 <= e2  = True
+    | otherwise = False
 
 {-
  - Realiza a soma deste Bag com otherBag. A soma de dois bags contem os elementos dos dois bags com suas quantidades somadas. 
 -}
-sum bag1 bag2 = undefined
+sum :: Map Int Int -> Map Int Int -> Map Int Int 
+sum bag1 bag2 = unionWith (+) bag1 bag2
 
 {-
  - Retorna a quantidade total de elementos no Bag
 -}
-size bag = undefined
+size :: Map Int Int -> Int
+size bag = Map.foldr (+) 0 bag
